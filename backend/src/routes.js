9ab8dec105e726c4b92d81d74e6314fe9b0e274b
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-11-13 12:31:26 -0300

    Adding Product table

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ import UserController from './app/controllers/UserController';
 import ClientController from './app/controllers/ClientController';
 import OrderController from './app/controllers/OrderController';
 import AddressController from './app/controllers/AddressController';
+import ProductController from './app/controllers/ProductController';
 
 import authMidlleware from './app/middleware/auth';
 
@@ -23,5 +24,6 @@ routes.put('/clients:id', ClientController.update);
 routes.post('/clients/:client_id/addresses', AddressController.store);
 
 routes.post('/clients/:client_id/orders', OrderController.store);
+routes.post('/orders/:order_id/products', ProductController.store);
 
 export default routes;
