6fd342ce57104fd6ab38cb62d2b7a9459fc03f41
Renan Rollo <renanrollo@MacBook-Pro-de-Renan.local>
2019-11-12 22:54:51 -0300

    backend

---
+++ backend/src/database/migrations/20191112210025-create-products.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('orders', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      value: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('orders');
+  },
+};
